6c53df240818da92f0c4efce99a8bc5354668afb
Aleksey Konchyts <konchits.aleksey@gmail.com>
2024-02-02 13:25:00 +0300

    Color change logic

--- RGBChangeColor/ViewController.swift
+++ RGBChangeColor/ViewController.swift
@@ -41,7 +41,7 @@ final class ViewController: UIViewController {
             red: CGFloat(redColorSlider.value),
             green: CGFloat(greenColorSlider.value),
             blue: CGFloat(blueColorSlider.value),
-            alpha: 1.0)
+            alpha: 0.9)
     }
 }
 
